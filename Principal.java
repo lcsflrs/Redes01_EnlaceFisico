aba57408b2903f25006139bc7a6eb433bef36b56
Lucas Flores <lcsflrs@gmail.com>
2021-07-30 11:23:34 -0300

    Correcao da Transformacao para Bits Implementada

--- Principal.java
+++ Principal.java
@@ -18,32 +18,47 @@ public class Principal {
     String[] quadro = new String[mensagem.length()];
     for (int i = 0; i < mensagem.length(); i++) {
       char converterAscii = mensagem.charAt(i);
-      // System.out.println("\n\nConverterAscii: " + converterAscii);
+      System.out.println("\n\nConverterAscii: " + converterAscii);
       int codigoAscii = (int) converterAscii;
-      // System.out.println("CodigoAscii: " + codigoAscii);
-      // System.out.println("ToBinaryString: " + Integer.toBinaryString(codigoAscii));
+      System.out.println("CodigoAscii: " + codigoAscii);
+      String inserir = Integer.toBinaryString(codigoAscii);
+      System.out.println("ToBinaryString: " + Integer.toBinaryString(codigoAscii));
       // System.out.println("ParseInt: " +
       // Integer.parseInt(Integer.toBinaryString(codigoAscii)));
-      quadro[i] = Integer.toBinaryString(codigoAscii);
+      if (inserir.length() < 8) {
+        System.out.println("\n");
+        while (inserir.length() < 8) {
+          System.out.println("Diferenca a 8 bits: " + (8 - inserir.length()));
+          String zero = "0";
+          inserir = zero.concat(inserir);
+        }
+        System.out.println("Saiu do IF \n");
+
+      }
+      System.out.println("ToBinaryString Com Zero: " + inserir);
+      quadro[i] = inserir;
     }
 
-    int[] bits = new int[quadro.length * 7];
+    int[] bits = new int[quadro.length * 8];
     for (int i = 0; i < quadro.length; i++) {
-      // System.out.println("\n\nStringAtual: " + quadro[i]);
+      System.out.println("\n\nStringAtual: " + quadro[i]);
       for (int k = 0; k < quadro[i].length(); k++) {
-        // System.out.println("\n\nCharAt: " + quadro[i].charAt(k));
-        // System.out.println("Char Get Numeric: " +
-        // Character.getNumericValue(quadro[i].charAt(k)));
-        bits[i * 7 + k] = Character.getNumericValue(quadro[i].charAt(k));
+        System.out.println("\n\nIndex: " + k);
+        System.out.println("CharAt: " + quadro[i].charAt(k));
+        System.out.println("Char Get Numeric: " + Character.getNumericValue(quadro[i].charAt(k)));
+        bits[i * 8 + k] = Character.getNumericValue(quadro[i].charAt(k));
       }
     }
 
-    /*
-     * System.out.println("\n\n"); for (int i = 0; i < quadro.length; i++) {
-     * System.out.print(quadro[i]); }
-     * 
-     * System.out.println("\n\n"); for (int i = 0; i < bits.length; i++) {
-     * System.out.print(bits[i]); }
-     */
+    System.out.println("\n\n");
+    for (int i = 0; i < quadro.length; i++) {
+      System.out.print(quadro[i]);
+    }
+
+    System.out.println("\n\n");
+    for (int i = 0; i < bits.length; i++) {
+      System.out.print(bits[i]);
+    }
+
   }
 }
